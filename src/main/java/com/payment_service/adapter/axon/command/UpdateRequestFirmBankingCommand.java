a1ec85e06c1764269afe92899f806a168fe26105
huncozyboy <leejh5838@naver.com>
2025-11-26 12:00:00 +0900

    feat: payment_service에서 사용할 axon 커맨드 구현

--- src/main/java/com/payment_service/adapter/axon/command/UpdateRequestFirmBankingCommand.java
+++ src/main/java/com/payment_service/adapter/axon/command/UpdateRequestFirmBankingCommand.java
@@ -1,4 +1,26 @@
 package com.payment_service.adapter.axon.command;
 
-public class UpdateRequestFirmBankingCommand {
+import jakarta.validation.constraints.NotNull;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import org.axonframework.modelling.command.TargetAggregateIdentifier;
+
+import com.payment_service.common.SelfValidating;
+
+@Builder
+@Data
+@EqualsAndHashCode(callSuper = false)
+public class UpdateRequestFirmBankingCommand extends SelfValidating<UpdateRequestFirmBankingCommand> {
+	@NotNull
+	@TargetAggregateIdentifier
+	private String aggregateIdentifier;
+	private final int firmBankingStatus;
+
+	public UpdateRequestFirmBankingCommand(String aggregateIdentifier, int firmBankingStatus) {
+		this.aggregateIdentifier = aggregateIdentifier;
+		this.firmBankingStatus = firmBankingStatus;
+
+		this.validateSelf();
+	}
 }
