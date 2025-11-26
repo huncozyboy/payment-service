ec74566d57316a1e2454e0c5006ba3474c699377
huncozyboy <leejh5838@naver.com>
2025-11-26 12:00:00 +0900

    feat: saga 커맨드 구현

---
+++ src/main/java/com/payment_service/adapter/axon/aggregate/RegisteredBankAccountAggregate.java
@@ -0,0 +1,4 @@
+package com.payment_service.adapter.axon.aggregate;
+
+public class RegisteredBankAccountAggregate {
+}
---
+++ src/main/java/com/payment_service/adapter/axon/command/CreateRequestFirmBankingCommand.java
@@ -0,0 +1,4 @@
+package com.payment_service.adapter.axon.command;
+
+public class CreateRequestFirmBankingCommand {
+}
