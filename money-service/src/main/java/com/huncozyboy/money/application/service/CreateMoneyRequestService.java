22f68ce9233d703c6929c04672480121b1dc01df
huncozyboy <leejh5838@naver.com>
2025-12-04 12:00:00 +0900

    feat: money-service 비즈니스 로직 서비스단 구현

--- money-service/src/main/java/com/huncozyboy/money/application/service/CreateMoneyRequestService.java
+++ money-service/src/main/java/com/huncozyboy/money/application/service/CreateMoneyRequestService.java
@@ -1,4 +1,35 @@
 package com.huncozyboy.money.application.service;
 
-public class CreateMoneyRequestService {
+import com.huncozyboy.common.UseCase;
+import com.huncozyboy.money.adapter.axon.command.CreateMemberMoneyCommand;
+import com.huncozyboy.money.application.port.in.CreateMemberMoneyRequestCommand;
+import com.huncozyboy.money.application.port.in.CreateMemberMoneyRequestUseCase;
+import com.huncozyboy.money.application.port.out.CreateMemberMoneyPort;
+import com.huncozyboy.money.domain.MemberMoney.MembershipId;
+import com.huncozyboy.money.domain.MemberMoney.MoneyAggregateIdentifier;
+import lombok.RequiredArgsConstructor;
+import org.axonframework.commandhandling.gateway.CommandGateway;
+
+@UseCase
+@RequiredArgsConstructor
+public class CreateMoneyRequestService implements CreateMemberMoneyRequestUseCase {
+
+	private final CreateMemberMoneyPort createMemberMoneyPort;
+	private final CommandGateway commandGateway;
+
+	@Override
+	public void createMoney(CreateMemberMoneyRequestCommand command) {
+		CreateMemberMoneyCommand axonCommand = new CreateMemberMoneyCommand(
+			command.getTargetMembershipId());
+		commandGateway.send(axonCommand).whenComplete((result, throwable) -> {
+			if (throwable != null) {
+				throw new RuntimeException(throwable);
+			} else {
+				createMemberMoneyPort.createMemberMoney(
+					new MembershipId(command.getTargetMembershipId()),
+					new MoneyAggregateIdentifier(result.toString())
+				);
+			}
+		});
+	}
 }
--- money-service/src/main/java/com/huncozyboy/money/application/service/IncreaseMoneyRequestService.java
+++ money-service/src/main/java/com/huncozyboy/money/application/service/IncreaseMoneyRequestService.java
@@ -1,4 +1,41 @@
 package com.huncozyboy.money.application.service;
 
-public class IncreaseMoneyRequestService {
+import com.huncozyboy.common.UseCase;
+import com.huncozyboy.money.adapter.axon.command.RechargingMoneyRequestCreateCommand;
+import com.huncozyboy.money.application.port.in.IncreaseMoneyRequestCommand;
+import com.huncozyboy.money.application.port.in.IncreaseMoneyRequestUseCase;
+import com.huncozyboy.money.application.port.out.GetMemberMoneyPort;
+import com.huncozyboy.money.domain.MemberMoney;
+import com.huncozyboy.money.domain.MemberMoney.MembershipId;
+import java.util.UUID;
+import lombok.RequiredArgsConstructor;
+import org.axonframework.commandhandling.gateway.CommandGateway;
+
+@UseCase
+@RequiredArgsConstructor
+public class IncreaseMoneyRequestService implements IncreaseMoneyRequestUseCase {
+
+	private final GetMemberMoneyPort getMemberMoneyPort;
+	private final CommandGateway commandGateway;
+
+	@Override
+	public void increaseMoneyRequest(IncreaseMoneyRequestCommand command) {
+		MemberMoney memberMoney = getMemberMoneyPort.getMemberMoney(
+			new MembershipId(command.getTargetMembershipId()));
+		String moneyIdentifier = memberMoney.getAggregateIdentifier();
+
+		RechargingMoneyRequestCreateCommand rechargingMoneyRequestCreateCommand = RechargingMoneyRequestCreateCommand.builder()
+			.aggregateIdentifier(moneyIdentifier)
+			.rechargingRequestId(UUID.randomUUID().toString())
+			.membershipId(command.getTargetMembershipId())
+			.amount(command.getAmount())
+			.build();
+
+		commandGateway.send(rechargingMoneyRequestCreateCommand)
+			.whenComplete((Object result, Throwable throwable) -> {
+				if (throwable != null) {
+					throw new RuntimeException(throwable);
+				}
+			});
+	}
 }
