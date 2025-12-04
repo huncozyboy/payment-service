b845fefe34568ac4dda63ebb1c237370d9f05f54
huncozyboy <leejh5838@naver.com>
2025-12-04 12:00:00 +0900

    feat: money-service 도메인 추가

--- money-service/src/main/java/com/huncozyboy/money/domain/MemberMoney.java
+++ money-service/src/main/java/com/huncozyboy/money/domain/MemberMoney.java
@@ -1,4 +1,70 @@
 package com.huncozyboy.money.domain;
 
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Value;
+
+@Getter
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class MemberMoney {
+
+	private final String memberMoneyId;
+	private final String membershipId;
+	private final int balance;
+	private final String aggregateIdentifier;
+
+	public static MemberMoney generateMemberMoney(
+		MemberMoneyId memberMoneyId,
+		MembershipId membershipId,
+		MoneyBalance moneyBalance,
+		MoneyAggregateIdentifier moneyAggregateIdentifier
+	) {
+		return new MemberMoney(
+			memberMoneyId.memberMoneyId,
+			membershipId.membershipId,
+			moneyBalance.balance,
+			moneyAggregateIdentifier.getAggregateIdentifier()
+		);
+	}
+
+	@Value
+	public static class MemberMoneyId {
+
+		public MemberMoneyId(String value) {
+			this.memberMoneyId = value;
+		}
+
+		String memberMoneyId;
+	}
+
+	@Value
+	public static class MembershipId {
+
+		public MembershipId(String value) {
+			this.membershipId = value;
+		}
+
+		String membershipId;
+	}
+
+	@Value
+	public static class MoneyBalance {
+
+		public MoneyBalance(int value) {
+			this.balance = value;
+		}
+
+		int balance;
+	}
+
+	@Value
+	public static class MoneyAggregateIdentifier {
+
+		public MoneyAggregateIdentifier(String value) {
+			this.aggregateIdentifier = value;
+		}
+
+		String aggregateIdentifier;
+	}
 }
